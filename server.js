67fcb6b0bc1ff877490ad159ce153a2d082fa6a2
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-09 17:19:38 -0300

    Criação da model e requireDir

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi_rocketseat', {
+    useNewUrlParser: true
+});
+requireDir('./src/models')
+
+const Product = mongoose.model('Product');
+
+// Criando a primeira rota.
 app.get('/', (req, res) => {
+    Product.create({
+        title: 'Rocket Native',
+        description: 'Build native apps with React',
+        url: 'https://github.com/facebook/react-native'
+    })
+
     res.send('Hello World!');
 })
 
+// Criando uma porta e inicializando
 app.listen(3001);
